c4794cc40afae016e5ae63db6724d827c160dce7
smolprotecc <85084644+smolprotecc@users.noreply.github.com>
2022-06-12 17:22:45 +1000

    Update frwc64.base.renderWorker.js

--- functions/frwc64.base.renderWorker.js
+++ functions/frwc64.base.renderWorker.js
@@ -2,28 +2,24 @@ let canvas;
 let context;
 
 let render = (function() {
-  let next = function(list, dimensions) {
-    if (list.length > 0) {
-      renderElements(list, dimensions)
-    } else {
-      exit()
-    }
-  }
-    
   let renderElements = async function(list, dimensions) {
     // https://stackoverflow.com/questions/56553281/webworker-offscreencanvas-draw-regular-image/56553680#56553680
     // https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
     let element = list.shift()
+    console.log(list)
+    console.log(element)
     
     const blob = await fetch(element).then(res => res.blob());
     const image = await createImageBitmap(blob);
     context.drawImage(image, dimensions.x, dimensions.y, dimensions.width, dimensions.height);
-    next(list, dimensions);
-  }
-  
-  let exit = function() {
-    const bitmap = canvas.transferToImageBitmap();
-    self.postMessage({msg: 'render', bitmap});
+    // iterate
+    if (list.length > 0) {
+      renderElements(list, dimensions)
+    } else {
+    // exit
+      const bitmap = canvas.transferToImageBitmap();
+      self.postMessage({msg: 'render', bitmap});
+    }
   }
   
   let start = function(list, options) {
