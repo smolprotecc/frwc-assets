10acc21cb9f39abb8002a86aa9cfe1d7b9ef2eaf
smolprotecc <85084644+smolprotecc@users.noreply.github.com>
2022-06-12 19:11:10 +1000

    Update frwc64.base.render.js

--- functions/frwc64.base.render.js
+++ functions/frwc64.base.render.js
@@ -7,20 +7,18 @@ frwc64.base64 = typeof frwc64.base64 != 'undefined' ? frwc64.base64 : {}
 
 frwc64.base64.render = function(list, target, options) {
   let canvas = document.querySelector(target);
-  // let context = canvas.getContext('bitmaprenderer');
-  // let offscreenCanvas = new OffscreenCanvas(canvas.width, canvas.height);
-  const offscreenCanvas = canvas.transferControlToOffscreen();
+  let context = canvas.getContext('bitmaprenderer');
+  let offscreenCanvas = new OffscreenCanvas(canvas.width, canvas.height);
+  // const offscreenCanvas = canvas.transferControlToOffscreen();
   
   let worker = new Worker(workerFile);
-  /*
   worker.addEventListener('message', function(ev) {    
-    console.log(ev.data)
-    context.transferFromImageBitmap(ev.data)
+    // console.log(ev.data)
+    // context.transferFromImageBitmap(ev.data)
     if (ev.data.msg === 'render') {
-      // console.log(ev.data.bitmap)
-      // context.transferFromImageBitmap(ev.data.bitmap);
+      console.log(ev.data.bitmap)
+      context.transferFromImageBitmap(ev.data.bitmap);
     }
   });
-  */
   worker.postMessage({msg: 'init', canvas: offscreenCanvas, list: list, options: options}, [offscreenCanvas]);
 }
