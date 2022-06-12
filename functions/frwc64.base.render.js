5f2c8877fcad417d0a4ae56e31cedd4cdf4d30ff
smolprotecc <85084644+smolprotecc@users.noreply.github.com>
2022-06-12 14:33:23 +1000

    Update frwc64.base.render.js

--- functions/frwc64.base.render.js
+++ functions/frwc64.base.render.js
@@ -6,19 +6,6 @@ frwc64 = typeof frwc64 != 'undefined' ? frwc64 : {}
 frwc64.base64 = typeof frwc64.base64 != 'undefined' ? frwc64.base64 : {}
 
 frwc64.base64.render = function(list, target, options) {
-  // check options
-  let x = 0;
-  let y = 0;
-  let width = 0;
-  let height = 0;
-  if (options) {
-    x = options.x || x
-    y = options.y || y
-    width = options.width || width
-    height = options.height || height
-  }
-  
-  // reference the target
   let canvas = document.querySelector(target);
   let context = canvas.getContext();
   let offscreenCanvas = new OffscreenCanvas(canvas.width, canvas.height);
