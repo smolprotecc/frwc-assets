cf2180131372a9320099196da09a821caed87117
smolprotecc <85084644+smolprotecc@users.noreply.github.com>
2022-06-12 18:12:52 +1000

    Update frwc64.base.render.js

--- functions/frwc64.base.render.js
+++ functions/frwc64.base.render.js
@@ -14,8 +14,9 @@ frwc64.base64.render = function(list, target, options) {
   worker.addEventListener('message', function(ev) {
     if(ev.data.msg === 'render') {
       console.log(ev.data)
-      console.log(ev.data.bitmap)
-      context.transferFromImageBitmap(ev.data.bitmap);
+      context.transferFromImageBitmap(ev.data)
+      // console.log(ev.data.bitmap)
+      // context.transferFromImageBitmap(ev.data.bitmap);
     }
   });
 }
