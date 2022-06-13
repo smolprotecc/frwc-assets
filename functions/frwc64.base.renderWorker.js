b3ab9f0b5afcf725f627c68dee51947ec16e25ab
smolprotecc <85084644+smolprotecc@users.noreply.github.com>
2022-06-13 10:51:13 +1000

    Update frwc64.base.renderWorker.js

--- functions/frwc64.base.renderWorker.js
+++ functions/frwc64.base.renderWorker.js
@@ -1,26 +1,30 @@
 let canvas;
 let context;
+let datum = {};
 
 let render = (function() {
-  let renderElements = async function(list, dimensions) {
+  let renderElements = async function(identifier, list, dimensions) {
     // https://stackoverflow.com/questions/56553281/webworker-offscreencanvas-draw-regular-image/56553680#56553680
     // https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
     let element = list.shift()
     
     const blob = await fetch(element).then(res => res.blob());
     const image = await createImageBitmap(blob);
-    context.drawImage(image, dimensions.x, dimensions.y, dimensions.width, dimensions.height);
+    datum[identifier].context.drawImage(image, dimensions.x, dimensions.y, dimensions.width, dimensions.height);
     // iterate
     if (list.length > 0) {
-      await renderElements(list, dimensions)
+      await renderElements(identifier, list, dimensions)
     } else {
     // exit
-      const bitmap = canvas.transferToImageBitmap();
-      self.postMessage({msg: 'render', bitmap: bitmap}, [bitmap]);
+      const bitmap = datum[identifier].canvas.transferToImageBitmap();
+      self.postMessage({msg: 'render', identifier: identifier, bitmap: bitmap}, [bitmap]);
     }
   }
   
-  let start = async function(list, options) {
+  let start = async function(identifier) {
+    let list = datum[identifier].list
+    let options = datum[identifier].options
+    
     let x = 0; 
     let y = 0;
     let width, height;
@@ -31,7 +35,7 @@ let render = (function() {
      height = options.height || height
     }
     // Start recursion
-    await renderElements(list, {x: x, y: y, width: width, height: height})
+    await renderElements(identifier, list, {x: x, y: y, width: width, height: height})
   }
   
   return {
@@ -41,11 +45,19 @@ let render = (function() {
 
 self.onmessage = function(ev) {
   if (ev.data.msg === 'init') {
+    datum[ev.data.identifier] = {
+      list: ev.data.list,
+      options: ev.data.options,
+      canvas : ev.data.canvas,
+      context: ev.data.canvas.getContext('2d'),
+    }
+    /*
     list = ev.data.list;
     options = ev.data.options;
     
     canvas = ev.data.canvas;
     context = canvas.getContext('2d');
-    render.start(list, options)
+    */
+    render.start(ev.data.identifier)
   }
 }
